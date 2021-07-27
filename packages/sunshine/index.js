cc6b2445ea34e43b9aa70470b5a1c17e91005658
向日葵Sun <1847159899@qq.com>
2021-07-27 14:22:30 +0800

    1

--- packages/sunshine/index.js
+++ packages/sunshine/index.js
@@ -1,16 +1,5 @@
 import components from './src/components.js'
 
-// const SunshineUI = Object.create(null)
-
-// SunshineUI.install = Vue => {
-//     for (const key in components){
-//         const component = components[key]
-//         Vue.component(component.name, component)
-//     }
-// }
-
-// export default SunshineUI
-
 export const install = Vue => {
     for (const key in components){
         const component = components[key]
