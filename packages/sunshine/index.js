f0963568311239199b8ce204923dfbd7268769fb
向日葵Sun <1847159899@qq.com>
2021-07-27 13:34:13 +0800

    1

--- packages/sunshine/index.js
+++ packages/sunshine/index.js
@@ -1,1 +1,12 @@
-export { default as SunButton } from '@liumingqin/button/button'
+import components from './components.js'
+
+const SunshineUI = Object.create(null)
+
+SunshineUI.install = Vue => {
+    for (const key in components){
+        const component = components[key]
+        Vue.component(component.name, component)
+    }
+}
+
+export default SunshineUI
---
+++ packages/sunshine/src/components.js
@@ -0,0 +1,1 @@
+export { default as SunButton } from '@liumingqin/button'
